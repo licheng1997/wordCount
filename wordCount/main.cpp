e81be47632a766fd704bb128f27427712025ef32
1hurricane <55039856+1hurricane@users.noreply.github.com>
2019-10-19 11:28:36 +0800

    Add files via upload

---
+++ wordCount/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include "characters1.h"
+//#include "wordcount.h"
+//#include "count_line.h"
+using namespace std;
+int main(int argc,char *argv[])
+{
+	ifstream infile;
+	ofstream outfile;
+	characters();
+	//WordCount();
+	//count_line();
+ } 
